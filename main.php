8c8bc8cedf518a6f22ae5102d9935a265a9c2ca2
Florencia Lagos <florencia.lagosamartino@mercadolibre.com>
2019-04-25 21:59:16 -0300

    tabla

--- main.php
+++ main.php
@@ -1,8 +1,46 @@
 <html>
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border: 1px solid #dddddd;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+    </style>
+</head>
     <body>
-        <?php
-        echo "<h1>Hola Mundo</h1>"
-        ?>
+    <table>
+        <tr>
+            <th>Personaje</th>
+            <th>Vive</th>
+            <th>Muere</th>
+            <th>WhiteWalker</th>
+        </tr>
+        <tr>
+            <td>Jon Snow</td>
+            <td>x</td>
+            <td>x</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Daenerys Targaryen</td>
+            <td>x</td>
+            <td>x</td>
+            <td>x</td>
+        </tr>
+        <tr>
+            <td>Sansa Stark</td>
+            <td>x</td>
+            <td>x</td>
+            <td><?php echo "<h1>Hola</h1>" ?></td>
+        </tr>
+    </table>
     </body>
 </html>
 
