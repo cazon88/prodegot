c893e5c63aaff0faf4b9cb6fbc1fcfec45673609
Florencia Lagos <florencia.lagosamartino@mercadolibre.com>
2019-04-25 23:21:14 -0300

    html del formulario

--- main.php
+++ main.php
@@ -4,6 +4,7 @@
         table {
             font-family: arial, sans-serif;
             border: 1px solid #dddddd;
+            margin-bottom: 20px;
         }
 
         td, th {
@@ -15,32 +16,211 @@
     </style>
 </head>
     <body>
-    <table>
-        <tr>
-            <th>Personaje</th>
-            <th>Vive</th>
-            <th>Muere</th>
-            <th>WhiteWalker</th>
-        </tr>
-        <tr>
-            <td>Jon Snow</td>
-            <td>x</td>
-            <td>x</td>
-            <td>x</td>
-        </tr>
-        <tr>
-            <td>Daenerys Targaryen</td>
-            <td>x</td>
-            <td>x</td>
-            <td>x</td>
-        </tr>
-        <tr>
-            <td>Sansa Stark</td>
-            <td>x</td>
-            <td>x</td>
-            <td><?php echo "<h1>Hola</h1>" ?></td>
-        </tr>
-    </table>
+        <table>
+            <tr>
+                <td> Usiario </td>
+                <td> <input type="text"> </td>
+                <td> <input type="submit"> </td>
+            </tr>
+        </table>
+        <table style="width: 100%;">
+            <tr>
+                <td>¿Daenerys esta embarazada?</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Arya completa su lista?</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>¿Quien mata al Nigth King?</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>¿Quien se queda con el trono?</td>
+                <td><input type="text"></td>
+            </tr>
+        </table>
+        <table style="width: 100%;"> 
+            <tr>
+                <td>
+                    <table style="width: 100%;">
+                        <tr>
+                            <th>Personaje</th>
+                            <th>Vive</th>
+                            <th>Muere</th>
+                            <th>WhiteWalker</th>
+                        </tr>
+                        <tr>
+                            <td>Jon Snow</td>
+                            <td><input type="radio" name="JonVive" value="Masculino" /></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Daenerys Targaryen</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Sansa Stark</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Arya Stark</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Bran Stark</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Cersei Lannister</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Jaime Lannister</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Tyrion Lannister</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Theon Greyjoy</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Yara Greyjoy</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Euron Greyjoy</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Samwell Tarly</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Gilly</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                    </table>
+                </td>
+                <td>
+                    <table style="width: 100%;">
+                        <tr>
+                            <th>Personaje</th>
+                            <th>Vive</th>
+                            <th>Muere</th>
+                            <th>WhiteWalker</th>
+                        </tr>
+                        <tr>
+                            <td>Nigth King</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Jorah Mormont</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Melisandre</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>El Perro</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>La Montaña</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Lord Varys</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Brienne Of Tarth</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Podrick Payne</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Gendry Baratheon</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Gusano Gris</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Missandei</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Davos Seaworth</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                        <tr>
+                            <td>Bronn Stokeworth</td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                            <td><input type="radio"></td>
+                        </tr>
+                    </table>
+                </td> 
+            </tr>
+        </table>
     </body>
 </html>
 
