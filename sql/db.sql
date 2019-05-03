01ec400ba5b444d23433a724613506646ec1e4ce
Maintainer <email@email.com>
2019-05-03 20:12:04 -0300

    Update DB

--- sql/db.sql
+++ sql/db.sql
@@ -33,7 +33,10 @@ CREATE TABLE `status` (
 
 LOCK TABLES `status` WRITE;
 /*!40000 ALTER TABLE `status` DISABLE KEYS */;
-INSERT INTO `status` VALUES (1,'lives'),(2,'dies'),(3,'white walker');
+INSERT INTO `status` (`id`, `value`) VALUES
+(1, 'alive'),
+(2, 'dead'),
+(3, 'white-walker');
 /*!40000 ALTER TABLE `status` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,37 +61,37 @@ CREATE TABLE `users` (
 
 DROP TABLE IF EXISTS `prode`;
 CREATE TABLE `prode` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
-  `jon_snow` int(11) NOT NULL,
-  `daenerys_targaryen` int(11) NOT NULL,
-  `sansa_stark` int(11) NOT NULL,
-  `arya_stark` int(11) NOT NULL,
-  `bran_stark` int(11) NOT NULL,
-  `cersei_lannister` int(11) NOT NULL,
-  `jaime_lannister` int(11) NOT NULL,
-  `tyrion_lannister` int(11) NOT NULL,
-  `theon_greyjoy` int(11) NOT NULL,
-  `yara_greyjoy` int(11) NOT NULL,
-  `euron_greyjoy` int(11) NOT NULL,
-  `samwell_tarly` int(11) NOT NULL,
+  `jon` int(11) NOT NULL,
+  `daenerys` int(11) NOT NULL,
+  `sansa` int(11) NOT NULL,
+  `arya` int(11) NOT NULL,
+  `bran` int(11) NOT NULL,
+  `cersei` int(11) NOT NULL,
+  `jaime` int(11) NOT NULL,
+  `tyrion` int(11) NOT NULL,
+  `theon` int(11) NOT NULL,
+  `yara` int(11) NOT NULL,
+  `euron` int(11) NOT NULL,
+  `samwell` int(11) NOT NULL,
   `gilly` int(11) NOT NULL,
   `night_king` int(11) NOT NULL,
-  `jorah_mormont` int(11) NOT NULL,
+  `jorah` int(11) NOT NULL,
   `melisandre` int(11) NOT NULL,
-  `the_hound` int(11) NOT NULL,
-  `the_mountain` int(11) NOT NULL,
-  `lord_varys` int(11) NOT NULL,
-  `brienne_of_tarth` int(11) NOT NULL,
-  `podryck_payne` int(11) NOT NULL,
-  `gendry_baratheon` int(11) NOT NULL,
-  `gray_worm` int(11) NOT NULL,
-  `missandey` int(11) NOT NULL,
-  `davos_seaworth` int(11) NOT NULL,
-  `bronn_stokeworth` int(11) NOT NULL,
-  `beric_dondarrion` int(11) NOT NULL,
-  `tormund_giantsbane` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `hound` int(11) NOT NULL,
+  `mountain` int(11) NOT NULL,
+  `varys` int(11) NOT NULL,
+  `brienne` int(11) NOT NULL,
+  `podryck` int(11) NOT NULL,
+  `gendry` int(11) NOT NULL,
+  `grey_worm` int(11) NOT NULL,
+  `missandei` int(11) NOT NULL,
+  `davos` int(11) NOT NULL,
+  `bronn` int(11) NOT NULL,
+  `beric` int(11) NOT NULL,
+  `tormund` int(11) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
   PRIMARY KEY (`id`),
   UNIQUE KEY `usuario_UNIQUE` (`id_user`),
   CONSTRAINT `fk_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`)
@@ -98,6 +101,7 @@ DROP TABLE IF EXISTS `characters`;
 CREATE TABLE `characters` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
+  `short_name` varchar(45) NOT NULL,
   `id_status` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_status_idx` (`id_status`),
@@ -106,6 +110,34 @@ CREATE TABLE `characters` (
 
 LOCK TABLES `characters` WRITE;
 /*!40000 ALTER TABLE `characters` DISABLE KEYS */;
-INSERT INTO `characters` VALUES (1,'Jon Snow',1),(2,'Daenerys Targaryen',1),(3,'Sansa Stark',1),(4,'Arya Stark',1),(5,'Bran Stark',1),(6,'Cersei Lannister',1),(7,'Jaime Lannister',1),(8,'Tyrion Lannister',1),(9,'Theon Greyjoy',1),(10,'Yara Greyjoy',1),(11,'Euron Greyjoy',1),(12,'Samwell Tarly',1),(13,'Gilly',1),(14,'Night King',1),(15,'Melisandre',1),(16,'Jorah Mormont',1),(17,'The Hound',1),(18,'The Mountain',1),(19,'Lord Varys',1),(20,'Brienne of Tarth',1),(21,'Podryck Payne',1),(22,'Gendry Baratheon',1),(23,'Greyworm',1),(24,'Missandei',1),(25,'Davos Seaworth',1),(26,'Bronn Stokeworth',1),(27,'Beric Dondarrion',1),(28,'Tormund Giantsbane',1);
+INSERT INTO `characters` (`id`, `name`, `short_name`, `id_status`) VALUES
+(1, 'Jon Snow', 'jon', 1),
+(2, 'Daenerys Targaryen', 'danaerys', 1),
+(3, 'Sansa Stark', 'sansa', 1),
+(4, 'Arya Stark', 'arya', 1),
+(5, 'Bran Stark', 'bran', 1),
+(6, 'Cersei Lannister', 'cersei', 1),
+(7, 'Jaime Lannister', 'jaime', 1),
+(8, 'Tyrion Lannister', 'tyrion', 1),
+(9, 'Theon Greyjoy', 'theon', 1),
+(10, 'Yara Greyjoy', 'yara', 1),
+(11, 'Euron Greyjoy', 'euron', 1),
+(12, 'Samwell Tarly', 'samwell', 1),
+(13, 'Gilly', 'gilly', 1),
+(14, 'Night King', 'night_king', 1),
+(15, 'Melisandre', 'melisandre', 1),
+(16, 'Jorah Mormont', 'jorah', 1),
+(17, 'The Hound', 'hound', 1),
+(18, 'The Mountain', 'mountain', 1),
+(19, 'Lord Varys', 'varys', 1),
+(20, 'Brienne of Tarth', 'brienne', 1),
+(21, 'Podryck Payne', 'podryck', 1),
+(22, 'Gendry Baratheon', 'gendry', 1),
+(23, 'Greyworm', 'grey_worm', 1),
+(24, 'Missandei', 'missandei', 1),
+(25, 'Davos Seaworth', 'davos', 1),
+(26, 'Bronn Stokeworth', 'bronn', 1),
+(27, 'Beric Dondarrion', 'beric', 1),
+(28, 'Tormund Giantsbane', 'tormund', 1);
 /*!40000 ALTER TABLE `characters` ENABLE KEYS */;
 UNLOCK TABLES;
